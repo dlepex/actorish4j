775b6b79c23b29d22e8ab2c25ad00e9d65caf6a1
Denis Lepekhin <denis.lepekhin@gmail.com>
2017-11-04 08:33:23 +0300

    log string fix

--- src/main/java/github/jcext/internal/JcExtQueueProvider.java
+++ src/main/java/github/jcext/internal/JcExtQueueProvider.java
@@ -7,6 +7,6 @@ import java.util.concurrent.ArrayBlockingQueue;
 public class JcExtQueueProvider {
 
 	public <T> Queue<T> newPreallocatedQueue(int cap) {
-		return new ArrayBlockingQueue<T>(cap);
+		return new ArrayBlockingQueue<>(cap);
 	}
 }
